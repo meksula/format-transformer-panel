e02727a9eefb639dc8e872a97b1b388bfc5f0411
meksula <meksula.karol.93@gmail.com>
2021-07-21 17:30:02 +0200

    Converting format implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'format-transformer-panel';
+
+  input = '';
+  output = '';
+
+  formatFrom = 'JSON';
+  formatTo = 'JSON';
+
+  options: string[] = ["JSON", "YAML", "XML"];
+
+  constructor(private http: HttpClient) {}
+
+  convert() {
+    const encodedInput = btoa(this.input);
+    console.log(this.input);
+    console.log(encodedInput);
+    console.log('Converting from: ' + this.formatFrom + ', to: ' + this.formatTo);
+    const requestBody = {
+      from: this.formatFrom,
+      to: this.formatTo,
+      encodedData: encodedInput
+    }
+    const json = JSON.stringify(requestBody);
+    const corsHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+     this.http.post('http://format-transformer.io/api/v1/transformation', json, {headers: corsHeaders})
+              .subscribe(resp => {
+                let body = JSON.parse(JSON.stringify(resp));
+                this.output = atob(body.encodedData);
+               });
+    
+            // this.http.post('http://localhost:7000/api/v1/transformation', json, {headers: corsHeaders})
+            //  .subscribe(resp => {
+              // let body = JSON.parse(JSON.stringify(resp));
+              // this.output = atob(body.encodedData);
+            //  });
+  }
 }
